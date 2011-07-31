44934408191ab1a12900c60490c316fbc08c1034
Pino Kai Ubu <pino@ubuntu.com>
2011-07-31 08:57:12 -0700

    dd

--- Aprss_BeginningSpring2_cap03/src/main/java/com/apress/coupling/LooselyCoupled.java
+++ Aprss_BeginningSpring2_cap03/src/main/java/com/apress/coupling/LooselyCoupled.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class LooselyCoupled {
    private Transport transport;
-   
+   pippo
    public LooselyCoupled() {}
    
    
