bd9732ff54f8bfc70424e80feaf9d06df1385ceb
Pino K Win <pino@kaiser>
2011-08-19 12:10:57 -0700

    aggiunto in build.xml lo swap dell implementazione alla fine del wsdl2java

--- SpringInActionChapter10_remoting-jax-ws_SpringBeanAutowiringSupport/src/main/java/com/habuma/spitter/remoting/jaxws/SEI/SpitterServiceEndpointInterfImpl.java
+++ SpringInActionChapter10_remoting-jax-ws_SpringBeanAutowiringSupport/src/main/java/com/habuma/spitter/remoting/jaxws/SEI/SpitterServiceEndpointInterfImpl.java
@@ -1,5 +1,6 @@
 package com.habuma.spitter.remoting.jaxws.SEI;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -24,30 +25,58 @@ public class SpitterServiceEndpointInterfImpl implements SpitterServiceEndpointI
     
     
     
-	public void deleteSpittle( long spittleId) {
-	
-	}
-	
-	
-	public void addSpittle( @WebParam(name="spittle") Spittle spittle) {
-		// TODO Auto-generated method stub
-		
-	}
+    public java.util.List<Spittle> getSpittlesForSpitter( @WebParam(name="spitter")Spitter spitter) { 
+        LOG.info("Executing operation getSpittlesForSpitter");
+        System.out.println(spitter);
+        try {
+            java.util.List<Spittle> out = new ArrayList<Spittle>();
+            Spittle uno = new Spittle();
+            uno.setText("uno");
+            Spittle due = new Spittle();
+            uno.setText("due");            
+            out.add(uno);
+            out.add(due);            
+            return out;
+        } catch (java.lang.Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
 
 
+    public void addSpittle( @WebParam(name="spittle")Spittle spittle) { 
+        LOG.info("Executing operation addSpittle");
+        System.out.println(spittle);
+        try {
+        } catch (java.lang.Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
+
+ 
+    public void deleteSpittle( @WebParam(name="spittleid")long spittleid) { 
+        LOG.info("Executing operation deleteSpittle");
+        System.out.println(spittleid);
+        try {
+        } catch (java.lang.Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
 
 
-
-	public List<Spittle> getRecentSpittles(@WebParam(name="spittleCount") int spittleCount) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public List<Spittle> getSpittlesForSpitter(@WebParam(name="spitter") Spitter spitter) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public java.util.List<Spittle> getRecentSpittles( @WebParam(name="spittleCount")int spittleCount) { 
+        LOG.info("Executing operation getRecentSpittles");
+        System.out.println(spittleCount);
+        try {
+            java.util.List<Spittle> _return = null;
+            return _return;
+        } catch (java.lang.Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
 
 
 
